3f0b59f9e250bdb9aa47de59f8b6f15ff6005e3f
wilfredkisku <wilfred.kisku@gmail.com>
2021-05-08 23:55:12 +0530

    changes in utils

--- utilities/utilities.py
+++ utilities/utilities.py
@@ -6,6 +6,41 @@ import matplotlib.pyplot as plt
 import glob
 
 dirs = '/home/wilfred/Datasets/UCF-101'
+dirs_video = '/home/wilfred/Downloads/github/Python_Projects/videoPrediction/data/coastguard_cif.mp4'
+dirs_images = '/home/wilfred/Downloads/github/Python_Projects/videoPrediction/data/coastguard'
+
+def convert(path):
+    
+    list_categories = sorted(glob.glob(path+'/*'))
+    for l in list_categories:
+        print(len(glob.glob(l+'/*')))
+
+    return None
+def resizeImage(path):
+
+    files = sorted(glob.glob(path+'/*'))
+    
+    for f in files:
+        img = cv2.imread(f, cv2.IMREAD_UNCHANGED)
+        img = cv2.resize(img[:,:288,:], (96,96))
+        fname = os.path.splitext(f)[0][-10:]
+        print(fname)
+        cv2.imwrite(path+'/'+fname+'_scaled.jpg', img)
+    return None
+
+def extract(path):
+
+    video = cv2.VideoCapture(path)
+    success, image = video.read()
+    count = 0
+
+    while success:
+        cv2.imwrite('frame%s.jpg'%str(count).zfill(5), image)
+        success, image = video.read()
+        print('Frame read : ',success)
+        count += 1
+
+    return None
 
 def process():
     
@@ -20,4 +55,7 @@ def process():
 
 if __name__ == "__main__":
 
-    process()
+    #process()
+    #extract(dirs_video)
+    #resizeImage(dirs_images)
+    convert(dirs)
